87b27e081dc7802b719c2046bdd5a62a4f170d67
David <david13.amador@gmail.com>
2017-11-13 14:32:56 +0100

    Probando Cosas

--- configurar_cluster.sh
+++ configurar_cluster.sh
@@ -6,3 +6,14 @@ then
 	echo "Uso: $0 <fichero_configuracion>"
 	exit 1
 fi
+
+#Comprobar ficheros
+ficheros="configurar_mount.sh"
+
+for fich in $ficheros; do
+	if [ ! -f $fich ]
+	then
+		echo "El fichero "$fich" no esta disponible."
+		exit 1
+	fi
+done
