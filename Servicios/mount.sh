0a59d9c937b3c1ddb9d82aeeda2146920d28cb99
David <david13.amador@gmail.com>
2017-11-19 23:47:22 +0100

    Mount v1

--- Servicios/mount.sh
+++ Servicios/mount.sh
@@ -15,6 +15,7 @@ then
     exit 1
 fi
 
+echo "MOUNT: Iniciando Montaje"
 #Extraemos los datos necesarios para configurar el montaje
 linea=0
 for comand in $(cat $1); do
@@ -32,13 +33,8 @@ done
 
 #Comprobamos si el dispositivo ya esta montado
 #Si no esta montado, lo montamos
-montaje="$NOMBRE_DEL_DISPOSITIVO $PUNTO_DE_MONTAJE auto defaults,auto,rw 0 0";
-if [ grep -q "$NOMBRE_DEL_DISPOSITIVO" /etc/fstab ]
-then
-	echo "El dispositivo ya esta montado en fstab"
-elif [ ! grep -q "$NOMBRE_DEL_DISPOSITIVO" /etc/fstab ]
-	'mount $NOMBRE_DEL_DISPOSITIVO $PUNTO_DE_MONTAJE' && echo "#Dispositivo: $NOMBRE_DEL_DISPOSITIVO" >> /etc/fstab && echo "$montaje" >> /etc/fstab && echo "Montaje realizado correctamente"
-then
-else
-	echo "Error al montar el dispositivo"
-fi
+echo "MOUNT: Intentando montar el dispositivo $NOMBRE_DEL_DISPOSITIVO en $PUNTO_DE_MONTAJE"
+montaje="$NOMBRE_DEL_DISPOSITIVO     $PUNTO_DE_MONTAJE   auto    auto    0   0";
+
+grep -q "$NOMBRE_DEL_DISPOSITIVO" /etc/fstab && echo "MOUNT: El dispositivo $NOMBRE_DEL_DISPOSITIVO ya esta en fstab" || (echo "$montaje" >> /etc/fstab && echo "MOUNT: Configuración del montaje de $NOMBRE_DEL_DISPOSITIVO completada") || echo "MOUNT: Error al montar el dispositivo $NOMBRE_DEL_DISPOSITIVO"
+mount -a > /dev/null
