5ac105caf7b3ce3696194afbf7d3d157e8dfcfd9
DannySanch <33498445+DannySanch@users.noreply.github.com>
2017-11-20 10:33:35 +0100

    Backup Server 2.0

--- Servicios/backup_server.sh
+++ Servicios/backup_server.sh
@@ -1,7 +1,19 @@
 #! /bin/bash
 
-
+# Archivo del backup del servidor
 # Recogemos la informacion necesaria para el perfil del servicio
-DIR = $(cat $1)
+
+echo '###### BACKUP ######'
+
+point = 0
+for argument in $(cat $); do
+	if [ $point = 0]; then
+		DIR = $argument
+	fi
+	let point+=1
+done
 echo 'CONFIG: Creando directorio de backup en $DIR'
+echo '#### FIN BACKUP ####'
+
+# Se crea directorio para backup
 mkdir -p $DIR
